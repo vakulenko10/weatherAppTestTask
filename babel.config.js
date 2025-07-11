5083f89afc839eb5c46e6f4cf17796642d73084c
Vitalik Vakulenko <121180497+vakulenko10@users.noreply.github.com>
2025-07-11 15:20:22 +0200

    tests configured and first test suite created

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+export default {
+  presets: [ ['@babel/preset-env', { targets: { node: 'current' } }],
+    ['@babel/preset-react', { runtime: 'automatic' }],
+    '@babel/preset-typescript',],
+};
